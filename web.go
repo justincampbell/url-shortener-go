5f6d3f9c01c9d84e30aa9902e5cafa2d964456fd
Justin Campbell <justin@justincampbell.me>
2013-04-29 08:59:59 -0400

    Allow specifying port

--- web.go
+++ web.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+  "flag"
   "fmt"
   "net/http"
 )
 
+var port = flag.String("port", "8080", "port to listen on")
+
+func init() {
+  flag.Parse()
+  fmt.Println("Listening on", *port)
+}
+
 func main() {
   http.HandleFunc("/test", handler)
-  http.ListenAndServe(":8080", nil)
+  http.ListenAndServe(":"+*port, nil)
 }
 
 func handler(response http.ResponseWriter, request *http.Request) {
   fmt.Fprintf(response, "test")
 }
-
