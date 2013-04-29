b9302562f746448a653d04dea5e56761afdf060f
Justin Campbell <justin@justincampbell.me>
2013-04-29 10:43:38 -0400

    Log port right before listen start

--- web.go
+++ web.go
@@ -10,11 +10,11 @@ var port = flag.String("port", "8080", "port to listen on")
 
 func init() {
   flag.Parse()
-  fmt.Println("Listening on", *port)
 }
 
 func main() {
   http.HandleFunc("/test", handler)
+  fmt.Println("Listening on", *port)
   http.ListenAndServe(":"+*port, nil)
 }
 
