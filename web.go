31c5a646cb2ba2f04bd1792d10f194532add7f9c
Justin Campbell <justin@justincampbell.me>
2013-04-30 20:30:24 -0400

    Create shortener

--- web.go
+++ web.go
@@ -4,21 +4,87 @@ import (
   "flag"
   "fmt"
   "net/http"
+  "strconv"
 )
 
-var port = flag.String("port", "8080", "port to listen on")
+var (
+  id   = 0
+  port = flag.String("port", "8080", "port to listen on")
+  urls = make(map[string]string)
+)
 
 func init() {
   flag.Parse()
 }
 
 func main() {
-  http.HandleFunc("/test", handler)
+  http.HandleFunc("/", expandHandler)
+  http.HandleFunc("/shorten", shortenHandler)
   fmt.Println("Listening on", *port)
   http.ListenAndServe(":"+*port, nil)
 }
 
-func handler(response http.ResponseWriter, request *http.Request) {
+func expandHandler(response http.ResponseWriter, request *http.Request) {
   fmt.Println(request.Method, request.RequestURI, request.RemoteAddr)
-  fmt.Fprintf(response, "test")
+
+  if request.RequestURI == "/" {
+    url := "http://github.com/justincampbell/url-shorteners"
+    http.Redirect(response, request, url, 301)
+    return
+  }
+
+  token := request.URL.Path[len("/"):]
+  url := expand(token)
+
+  if url == "" {
+    http.NotFound(response, request)
+    return
+  }
+
+  http.Redirect(response, request, url, 301)
+}
+
+func shortenHandler(response http.ResponseWriter, request *http.Request) {
+  fmt.Println(request.Method, request.RequestURI, request.RemoteAddr)
+  uri := request.RequestURI
+  parameter := "/shorten?url="
+
+  if len(uri) < len(parameter) {
+    http.Error(response, "Bad Request", 400)
+    return
+  }
+
+  url := uri[len(parameter):]
+
+  if url == "" {
+    http.Error(response, "Bad Request", 400)
+    return
+  }
+
+  token := shorten(url)
+  fmt.Fprintf(response, "/%s", token)
+}
+
+func expand(token string) string {
+  return urls[token]
+}
+
+func shorten(url string) string {
+  token := nextToken()
+  urls[token] = url
+  return token
+}
+
+func nextId() int {
+  id = id + 1
+  return id
+}
+
+func nextToken() string {
+  id := nextId()
+  return tokenize(id)
+}
+
+func tokenize(id int) string {
+  return strconv.Itoa(id)
 }
---
+++ web_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+  "github.com/bmizerany/assert"
+  "testing"
+)
+
+func reset() {
+  id = 0
+  urls = make(map[string]string)
+}
+
+func TestShorten(t *testing.T) {
+  reset()
+  url := "http://justincampbell.me"
+  token := "1"
+  assert.Equal(t, token, shorten(url))
+}
+
+func TestExpand(t *testing.T) {
+  url := "http://justincampbell.me"
+  token := "1"
+  urls[token] = url
+  assert.Equal(t, url, expand(token))
+}
+
+func TestNextId(t *testing.T) {
+  reset()
+  assert.Equal(t, 1, nextId())
+  assert.Equal(t, 2, nextId())
+}
+
+func TestNextToken(t *testing.T) {
+  reset()
+  assert.Equal(t, "1", nextToken())
+  assert.Equal(t, "2", nextToken())
+}
+
+func TestTokenize(t *testing.T) {
+  assert.Equal(t, "1", tokenize(1))
+}
